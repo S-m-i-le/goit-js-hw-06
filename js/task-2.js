3d9347699f524afac95d0441861b349085a92843
Andrey <artcity.kh@gmail.com>
2023-11-17 19:48:19 +0100

    HW-6

--- js/task-2.js
+++ js/task-2.js
@@ -1,60 +1,28 @@
-const getUsersWithFriend = (allUsers, friend) =>
-  allUsers.filter(users =>
-    Object.values(users.friends).some(user => user === friend),
-  );
-const allUsers = [
-  {
-    name: 'Moore Hensley',
-    friends: ['Sharron Pace'],
-  },
-  {
-    name: 'Sharlene Bush',
-    friends: ['Briana Decker', 'Sharron Pace'],
-  },
-  {
-    name: 'Ross Vazquez',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-  },
-  {
-    name: 'Elma Head',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-  },
-  {
-    name: 'Carey Barr',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-  },
-  {
-    name: 'Blackburn Dotson',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-  },
-  {
-    name: 'Sheree Anthony',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-  },
-];
-
-console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-console.log('--------------------------------');
-console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-console.log('--------------------------------');
-console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+    this.newItems = this.newItems;
+  }
+  // }
+  // повертає масив поточних товарів у приватній властивості items.
+  getItems() {
+    return this.#items;
+  }
+  //  приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+  addItem(newItem) {
+    this.#items.push(newItem);
+    return this.#items;
+  }
+  //  приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+    return this.#items;
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
